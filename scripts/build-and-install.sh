aaedcac91e30607c9dc4ca618793a05cf4432567
Joakim Frostegård <joakim.frostegard@gmail.com>
2019-05-04 18:58:58 +0200

    build-and-install.sh: check if target dir exists before rm, show message

--- scripts/build-and-install.sh
+++ scripts/build-and-install.sh
@@ -7,6 +7,12 @@ MOVE_TO="/Library/Audio/Plug-Ins/VST/FM_Rust/"
 
 cargo build --release
 ./scripts/osx_vst_bundler.sh "$NAME" target/release/libfm.dylib
-rm -r "$MOVE_TO"
+
+if [ -d "$MOVE_TO" ]; then
+    rm -r "$MOVE_TO"
+fi
+
 mkdir -p "$MOVE_TO"
 mv "$NAME.vst" "$MOVE_TO"
+
+echo "Copied VST bundle to $MOVE_TO"
