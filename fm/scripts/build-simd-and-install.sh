e40215e04c9ca6012ed4f6ff7f59b5189ae9da96
Joakim Frostegård <joakim.frostegard@gmail.com>
2019-06-18 14:15:47 +0200

    install script with simd: use dynamic sleef lib dir, print message

--- fm/scripts/build-simd-and-install.sh
+++ fm/scripts/build-simd-and-install.sh
@@ -10,12 +10,13 @@ NAME="FM Rust"
 
 VST_NAME="$NAME.vst"
 MOVE_TO="/Library/Audio/Plug-Ins/VST/$VST_NAME"
+SLEEF_DIR="$HOME/Projects/sleef/my-sleef-install/lib/"
 TMP_DIR="tmp"
 
 RUSTFLAGS="-C target-cpu=native" cargo +nightly build --release --features "simd"
 
 # TODO: static linking
-install_name_tool -add_rpath /Users/j/Projects/sleef/my-sleef-install/lib/ ../target/release/libfm.dylib
+install_name_tool -add_rpath "$SLEEF_DIR" ../target/release/libfm.dylib
 
 ./scripts/osx_vst_bundler.sh "$NAME" ../target/release/libfm.dylib
 
@@ -26,3 +27,4 @@ fi
 mv "$TMP_DIR/$VST_NAME" "$MOVE_TO"
 
 echo "Copied VST bundle to $MOVE_TO"
+echo "Currently requires sleef library in $SLEEF_DIR to run!"
