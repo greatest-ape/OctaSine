86b5a06dbf08465f3790bf4d3d5b475c3867f755
Joakim Frostegård <joakim.frostegard@gmail.com>
2019-06-30 10:44:29 +0200

    Link sleef statically by using sleef-sys fork

--- scripts/build-simd-and-install.sh
+++ scripts/build-simd-and-install.sh
@@ -10,15 +10,11 @@ NAME="OctaSine"
 
 VST_NAME="$NAME.vst"
 MOVE_TO="/Library/Audio/Plug-Ins/VST/$VST_NAME"
-SLEEF_DIR="$HOME/Projects/sleef/my-sleef-install/lib/"
 TMP_DIR="tmp"
 
 cd octasine_vst
 
 RUSTFLAGS="-C target-cpu=native" cargo +nightly build --release --features "simd logging"
-
-# TODO: static linking
-install_name_tool -add_rpath "$SLEEF_DIR" ../target/release/liboctasine.dylib
 
 ../scripts/osx_vst_bundler.sh "$NAME" ../target/release/liboctasine.dylib
 
@@ -29,4 +25,3 @@ fi
 mv "$TMP_DIR/$VST_NAME" "$MOVE_TO"
 
 echo "Copied VST bundle to $MOVE_TO"
-echo "Currently requires sleef library in $SLEEF_DIR to run!"
