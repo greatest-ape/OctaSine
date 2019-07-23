a1678af3dd3be0a24658fb4658a6ea486a14feaf
Joakim Frostegård <joakim.frostegard@gmail.com>
2019-07-23 23:44:34 +0200

    sin benchmark: test same values for both functions, improve code

--- octasine/benches/sin.rs
+++ octasine/benches/sin.rs
@@ -16,30 +16,26 @@ fn main(){
     use simd_sleef_sin35::SleefSin35;
     use packed_simd::*;
 
-    use octasine::constants::TAU;
-
     let mut rng = SmallRng::from_entropy();
 
-    let mut inputs = Vec::new();
-    let mut inputs_tau = Vec::new();
-    let mut inputs_simple = Vec::new();
+    let mut inputs_std: Vec<f64> = Vec::new();
+    let mut inputs_sleef: Vec<V> = Vec::new();
 
     let iterations = 50_000_000;
 
     for i in 0..iterations {
         let v = rng.gen::<f64>() * (((i % 256) + 1) as f64);
 
-        inputs_simple.push(v);
+        inputs_std.push(v);
 
         let v = V::new(v, v * 0.4, v, v * 0.1);
 
-        inputs.push(v);
-        inputs_tau.push(v * TAU);
+        inputs_sleef.push(v);
     }
 
     let start_sleef = Instant::now();
 
-    let outputs_sleef: Vec<V> = inputs_tau.into_iter()
+    let outputs_sleef: Vec<V> = inputs_sleef.into_iter()
         .map(|v| SleefSin35::sin(v))
         .collect();
 
@@ -47,7 +43,7 @@ fn main(){
 
     let start_simple = Instant::now();
 
-    let outputs_simple: Vec<f64> = inputs_simple.into_iter()
+    let outputs_std: Vec<f64> = inputs_std.into_iter()
         .map(|v| v.sin())
         .collect();
 
@@ -68,15 +64,15 @@ fn main(){
 
     let mut dummy_counter = 0usize;
 
-    let iterator = outputs_sleef.into_iter().zip(outputs_simple.into_iter());
+    let iterator = outputs_sleef.into_iter().zip(outputs_std.into_iter());
 
     for (a, b) in iterator {
-        if a.extract(0) == b {
+        if a.sum() == b {
             dummy_counter += 1;
         }
     }
 
-    if dummy_counter > iterations {
+    if dummy_counter > (iterations >> 2) {
         println!("Dummy information: {}", dummy_counter);
     }
 }
