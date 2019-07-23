ac5c97f5a9ceba20bb0e91befb508c869ab0ccd4
Joakim Frostegård <joakim.frostegard@gmail.com>
2019-07-23 19:11:11 +0200

    simd_sleef_sin35: use doubles, use AVX and AVX2 functions

--- simd_sleef_sin35/src/lib.rs
+++ simd_sleef_sin35/src/lib.rs
@@ -12,67 +12,100 @@ extern crate sleef_sys;
 
 use packed_simd::*;
 use sleef_sys::*;
+use cfg_if::cfg_if;
 
 
 #[allow(improper_ctypes)]
 extern "C" {
-    #[link_name = "llvm.sin.v4f32"]
-    fn sin_v4f32(x: f32x4) -> f32x4;
+    #[link_name = "llvm.sin.v4f64"]
+    fn sin_v4f64(x: f64x4) -> f64x4;
 
-    #[link_name = "llvm.sin.f32"]
-    fn sin_f32(x: f32) -> f32;
+    #[link_name = "llvm.sin.v2f64"]
+    fn sin_v2f64(x: f64x2) -> f64x2;
 }
 
 
 /// SIMD sine with 3.5 ULP error bound using the sleef library.
 /// 
-/// Resorts to standard LLVM sine when SSE4.2 is not available.
+/// Resorts to standard LLVM sine when SIMD is not available
 pub trait SleefSin35 {
     fn sin(self) -> Self;
 }
 
 
-impl SleefSin35 for f32x4 {
+impl SleefSin35 for f64x4 {
     #[inline]
     fn sin(self) -> Self {
+        cfg_if! {
+            if #[cfg(target_feature = "avx2")] {
+                #[inline]
+                unsafe fn f(a: f64x4) -> f64x4 {
+                    transmute(Sleef_sind4_u35avx2(transmute(a)))
+                }
+            } else if #[cfg(target_feature = "avx")] {
+                #[inline]
+                unsafe fn f(a: f64x4) -> f64x4 {
+                    transmute(Sleef_sind4_u35avx(transmute(a)))
+                }
+            } else if #[cfg(target_feature = "sse4.2")] {
+                #[inline]
+                unsafe fn f(a: f64x4) -> f64x4 {
+                    union U {
+                        vec: f64x4,
+                        halves: [f64x2; 2],
+                    }
+
+                    let mut halves = U { vec: a }.halves;
+
+                    *halves.get_unchecked_mut(0) =
+                        transmute(Sleef_sind2_u35sse4(transmute(*halves.get_unchecked(0))));
+                    *halves.get_unchecked_mut(1) =
+                        transmute(Sleef_sind2_u35sse4(transmute(*halves.get_unchecked(1))));
+
+                    U { halves }.vec
+                }
+            } else {
+                #[inline]
+                unsafe fn f(a: f64x4) -> f64x4 {
+                    return transmute(sin_v4f64(transmute(a)));
+                }
+            }
+        }
         unsafe {
-            #[cfg(target_feature = "sse4.2")]
-            return transmute(Sleef_sinf4_u35sse4(transmute(self)));
-            #[cfg(not(target_feature = "sse4.2"))]
-            return transmute(sin_v4f32(transmute(self)));
+            f(self)
         }
     }
 }
 
 
-impl SleefSin35 for f32x2 {
+impl SleefSin35 for f64x2 {
     #[inline]
     fn sin(self) -> Self {
+        cfg_if! {
+            if #[cfg(target_feature = "avx2")] {
+                #[inline]
+                unsafe fn f(a: f64x2) -> f64x2 {
+                    transmute(Sleef_sind2_u35avx2128(transmute(a)))
+                }
+            } else if #[cfg(target_feature = "avx")] {
+                #[inline]
+                unsafe fn f(a: f64x2) -> f64x2 {
+                    transmute(Sleef_sind2_u35sse4(transmute(a)))
+                }
+            } else if #[cfg(target_feature = "sse4.2")] {
+                #[inline]
+                unsafe fn f(a: f64x2) -> f64x2 {
+                    transmute(Sleef_sind2_u35sse4(transmute(a)))
+                }
+            } else {
+                #[inline]
+                unsafe fn f(a: f64x2) -> f64x2 {
+                    transmute(sin_v2f64(transmute(a)))
+                }
+            }
+        }
         unsafe {
-            #[cfg(target_feature = "sse4.2")]
-            return {
-                union U {
-                    vec: [f32x2; 2],
-                    twice: f32x4,
-                }
-
-                let twice = U { vec: [self, uninitialized()] }.twice;
-                let twice = transmute(Sleef_sinf4_u35sse4(transmute(twice)));
-
-                *(U { twice }.vec.get_unchecked(0))
-            };
-            #[cfg(not(target_feature = "sse4.2"))]
-            return {
-                union U {
-                    vec: f32x2,
-                    scalars: [f32; 2],
-                }
-                let mut scalars = U { vec: self }.scalars;
-                for i in &mut scalars {
-                    *i = sin_f32(*i);
-                }
-                U { scalars }.vec
-            };
+            f(self)
         }
     }
 }
@@ -85,10 +118,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sin(){
-        fn prop(value: f32) -> TestResult {
-            let x2 = f32x2::splat(value);
-            let x4 = f32x4::splat(value);
+    fn test_precision(){
+        fn prop(value: f64) -> TestResult {
+            let x2 = f64x2::splat(value);
+            let x4 = f64x4::splat(value);
 
             let test_sin_x2 = SleefSin35::sin(x2).extract(0);
             let test_sin_x4 = SleefSin35::sin(x4).extract(0);
@@ -98,7 +131,7 @@ mod tests {
             let reference_sin = value.sin();
             let diff = (test_sin_x2 - reference_sin).abs();
 
-            let success = diff < 0.000001;
+            let success = diff < 0.000000000000001;
 
             if !success {
                 println!();
@@ -111,6 +144,6 @@ mod tests {
             TestResult::from_bool(success)
         }
 
-        quickcheck(prop as fn(f32) -> TestResult);
+        quickcheck(prop as fn(f64) -> TestResult);
     }
 }
