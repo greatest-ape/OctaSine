e741a5ce9ef598b32ee79da56c03d00c634d1f9e
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-10-20 20:43:04 +0200

    Add debug build script: scripts/macos/build-dev-and-install.sh

---
+++ scripts/macos/build-dev-and-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Settings
+
+NAME="OctaSine"
+
+# Script
+
+VST_NAME="$NAME.vst"
+MOVE_TO="/Library/Audio/Plug-Ins/VST/$VST_NAME"
+TMP_DIR="tmp"
+
+cd octasine_vst
+
+cargo +stable build --features "logging"
+../contrib/osx_vst_bundler.sh "$NAME" ../target/debug/liboctasine.dylib
+
+if [ -d "$MOVE_TO" ]; then
+    rm -r "$MOVE_TO"
+fi
+
+if mv "$TMP_DIR/$VST_NAME" "$MOVE_TO"; then
+    echo "Copied VST bundle to $MOVE_TO"
+fi
