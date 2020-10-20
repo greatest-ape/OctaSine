629e8edf3e814101ecebd705fadb2cfd7838df5e
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-10-20 18:27:11 +0200

    GUI: get iced hello world up and running

--- octasine/src/gui/mod.rs
+++ octasine/src/gui/mod.rs
@@ -1,15 +1,17 @@
-// https://github.com/hecrj/iced/blob/0.1/examples/integration/src/main.rs
-// 
+// Interesting links:
+// - https://github.com/hecrj/iced/blob/0.1/examples/integration/src/main.rs
 
 use std::sync::Arc;
 
 use vst::editor::Editor;
-use vst_window::{EditorWindow, EventSource};
-
-mod interface;
 
 use super::SyncOnlyState;
-use interface::Interface;
+
+mod bridge;
+mod interface;
+
+use bridge::Handler;
+use interface::Application;
 
 
 const GUI_WIDTH: usize = 500;
@@ -18,7 +20,7 @@ const GUI_HEIGHT: usize = 500;
 
 pub struct Gui {
     sync_only: Arc<SyncOnlyState>,
-    opened_interface: Option<Interface>,
+    opened_interface: Option<Handler<Application>>,
 }
 
 
@@ -43,9 +45,17 @@ impl Editor for Gui {
 
     fn open(&mut self, parent: *mut ::core::ffi::c_void) -> bool {
         if self.opened_interface.is_none(){
-            let (window, event_source) = vst_window::setup(parent, self.size());
+            let (window, event_source) = vst_window::setup(
+                parent,
+                self.size()
+            );
 
-            let interface = Interface::new(window, event_source);
+            let interface = Handler::build(
+                window,
+                event_source,
+                GUI_WIDTH as u32,
+                GUI_HEIGHT as u32
+            );
             
             self.opened_interface = Some(interface);
 
@@ -67,5 +77,19 @@ impl Editor for Gui {
         if let Some(interface) = self.opened_interface.as_mut() {
             interface.process_events();
         }
+    }
+
+    #[cfg(feature = "logging")]
+    fn key_down(&mut self, keycode: vst::editor::KeyCode) -> bool {
+        ::log::info!("key down: {:?}", keycode);
+
+        true
+    }
+
+    #[cfg(feature = "logging")]
+    fn key_up(&mut self, keycode: vst::editor::KeyCode) -> bool {
+        ::log::info!("key up: {:?}", keycode);
+
+        true
     }
 }
