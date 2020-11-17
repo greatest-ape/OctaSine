a80a5d09502890ca9643355ff74efea5db71ae1b
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-11-17 21:28:40 +0100

    WIP: use baseview instead of vst_window

--- octasine/src/gui/mod.rs
+++ octasine/src/gui/mod.rs
@@ -3,7 +3,9 @@
 
 use std::sync::Arc;
 
+use baseview::{Parent, Size, WindowScalePolicy, Window, WindowOpenOptions};
 use vst::editor::Editor;
+use raw_window_handle::RawWindowHandle;
 
 use super::SyncOnlyState;
 
@@ -20,7 +22,8 @@ const GUI_HEIGHT: usize = 750;
 
 pub struct Gui {
     sync_only: Arc<SyncOnlyState>,
-    opened_interface: Option<Handler<Application>>,
+    opened: bool,
+    // opened_interface: Option<Handler<Application>>,
 }
 
 
@@ -28,7 +31,8 @@ impl Gui {
     pub fn new(sync_only: Arc<SyncOnlyState>) -> Self {
         Self {
             sync_only,
-            opened_interface: None,
+            opened: true,
+            // opened_interface: None,
         }
     }
 }
@@ -44,21 +48,18 @@ impl Editor for Gui {
     }
 
     fn open(&mut self, parent: *mut ::core::ffi::c_void) -> bool {
-        if self.opened_interface.is_none(){
-            let (window, event_source) = vst_window::setup(
-                parent,
-                self.size()
-            );
+        if !self.opened{
+            let window_options = WindowOpenOptions {
+                scale: WindowScalePolicy::SystemScaleFactor,
+                size: Size::new(GUI_WIDTH as f64, GUI_HEIGHT as f64),
+                parent: Parent::WithParent(raw_window_handle_from_parent(parent)),
+                title: crate::constants::PLUGIN_NAME.into(),
+            };
 
-            let interface = Handler::build(
-                window,
-                event_source,
-                GUI_WIDTH as u32,
-                GUI_HEIGHT as u32
-            );
+            let _ = Window::open( window_options, |window| {
+                Handler::<Application>::build(window, GUI_WIDTH as u32, GUI_HEIGHT as u32)
+            });
             
-            self.opened_interface = Some(interface);
-
             true
         } else {
             false
@@ -66,30 +67,50 @@ impl Editor for Gui {
     }
 
     fn close(&mut self) {
-        self.opened_interface = None;
+        // FIXME
+        self.opened = false;
     }
 
     fn is_open(&mut self) -> bool {
-        self.opened_interface.is_some()
+        self.opened
     }
+}
 
-    fn idle(&mut self) {
-        if let Some(interface) = self.opened_interface.as_mut() {
-            interface.process_events();
-        }
-    }
 
-    #[cfg(feature = "logging")]
-    fn key_down(&mut self, keycode: vst::editor::KeyCode) -> bool {
-        ::log::info!("key down: {:?}", keycode);
+#[cfg(target_os = "macos")]
+fn raw_window_handle_from_parent(
+    parent: *mut ::std::ffi::c_void
+) -> RawWindowHandle {
+    use raw_window_handle::macos::MacOSHandle;
 
-        true
-    }
+    RawWindowHandle::MacOS(MacOSHandle {
+        ns_view: parent,
+        ..MacOSHandle::empty()
+    })
+}
 
-    #[cfg(feature = "logging")]
-    fn key_up(&mut self, keycode: vst::editor::KeyCode) -> bool {
-        ::log::info!("key up: {:?}", keycode);
 
-        true
-    }
+#[cfg(target_os = "windows")]
+fn raw_window_handle_from_parent(
+    parent: *mut ::std::ffi::c_void
+) -> RawWindowHandle {
+    use raw_window_handle::windows::WindowsHandle;
+
+    RawWindowHandle::Windows(WindowsHandle {
+        hwnd: parent,
+        ..WindowsHandle::empty()
+    })
+}
+
+
+#[cfg(target_os = "linux")]
+fn raw_window_handle_from_parent(
+    parent: *mut ::std::ffi::c_void
+) -> RawWindowHandle {
+    use raw_window_handle::unix::XcbHandle;
+
+    RawWindowHandle::Xcb(XcbHandle {
+        window: parent as u32,
+        ..XcbHandle::empty()
+    })
 }
