2899d6782200e0fd403ee7dfea7f86c8d7257beb
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-12-02 22:11:23 +0100

    work on gui

--- octasine/src/gui/interface/operator.rs
+++ octasine/src/gui/interface/operator.rs
@@ -15,6 +15,8 @@ pub struct OperatorWidgets {
     volume: OctaSineKnob,
     panning: OctaSineKnob,
     frequency_ratio: OctaSineKnob,
+    frequency_free: OctaSineKnob,
+    frequency_fine: OctaSineKnob,
 }
 
 
@@ -23,11 +25,11 @@ impl OperatorWidgets {
         sync_handle: &Arc<H>,
         operator_index: usize,
     ) -> Self {
-        let (volume, panning, ratio) = match operator_index {
-            0 => (2, 3, 7),
-            1 => (15, 16, 21),
-            2 => (29, 30, 36),
-            3 => (44, 45, 52),
+        let (volume, panning, ratio, free, fine) = match operator_index {
+            0 => ( 2,  3,  7,  8, 9),
+            1 => (15, 16, 21, 22, 23),
+            2 => (29, 30, 36, 37, 38),
+            3 => (44, 45, 51, 52, 53),
             _ => unreachable!(),
         };
 
@@ -36,6 +38,8 @@ impl OperatorWidgets {
             volume: OctaSineKnob::operator_volume(sync_handle, volume),
             panning: OctaSineKnob::operator_panning(sync_handle, panning),
             frequency_ratio: OctaSineKnob::operator_frequency_ratio(sync_handle, ratio),
+            frequency_free: OctaSineKnob::operator_frequency_free(sync_handle, free),
+            frequency_fine: OctaSineKnob::operator_frequency_fine(sync_handle, fine),
         }
     }
 
@@ -48,7 +52,14 @@ impl OperatorWidgets {
             .push(Space::with_width(Length::Units(16)))
             .push(self.volume.view(sync_handle))
             .push(self.panning.view(sync_handle))
+            .push(
+                Container::new(
+                    Rule::vertical(16)
+                )
+                    .height(Length::Units(64)))
             .push(self.frequency_ratio.view(sync_handle))
+            .push(self.frequency_free.view(sync_handle))
+            .push(self.frequency_fine.view(sync_handle))
             .into()
     }
 }
