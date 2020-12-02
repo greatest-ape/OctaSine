2bee985e4a8740efc1a824156e95964da1aa8f62
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-12-02 21:56:16 +0100

    work on GUI

--- octasine/src/gui/interface/operator.rs
+++ octasine/src/gui/interface/operator.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use iced_baseview::{
-    Container, Column, Element, Text, Length, HorizontalAlignment, Align, Row, Rule
+    Container, Column, Element, Text, Length, HorizontalAlignment, Align, Row, Rule, Space
 };
 
 use crate::SyncHandle;
@@ -23,32 +23,32 @@ impl OperatorWidgets {
         sync_handle: &Arc<H>,
         operator_index: usize,
     ) -> Self {
+        let (volume, panning, ratio) = match operator_index {
+            0 => (2, 3, 7),
+            1 => (15, 16, 21),
+            2 => (29, 30, 36),
+            3 => (44, 45, 52),
+            _ => unreachable!(),
+        };
+
         Self {
             index: operator_index,
-            volume: OctaSineKnob::operator_volume(sync_handle, 2),
-            panning: OctaSineKnob::operator_panning(sync_handle, 3),
-            frequency_ratio: OctaSineKnob::operator_frequency_ratio(sync_handle, 7),
+            volume: OctaSineKnob::operator_volume(sync_handle, volume),
+            panning: OctaSineKnob::operator_panning(sync_handle, panning),
+            frequency_ratio: OctaSineKnob::operator_frequency_ratio(sync_handle, ratio),
         }
     }
 
     pub fn view<H: SyncHandle>(&mut self, sync_handle: &Arc<H>) -> Element<Message> {
-        let title = format!("Operator {}", self.index + 1);
+        let title = format!("{}", self.index + 1);
 
         Row::new()
-            .push(
-                Column::new()
-                    .spacing(16)
-                    .push(
-                        Row::new()
-                            .push(Text::new(title).size(16))
-                    )
-                    .push(
-                        Row::new()
-                            .push(self.volume.view(sync_handle))
-                            .push(self.panning.view(sync_handle))
-                            .push(self.frequency_ratio.view(sync_handle))
-                    )
-            )
+            .align_items(Align::Center)
+            .push(Text::new(title).size(32))
+            .push(Space::with_width(Length::Units(16)))
+            .push(self.volume.view(sync_handle))
+            .push(self.panning.view(sync_handle))
+            .push(self.frequency_ratio.view(sync_handle))
             .into()
     }
 }
