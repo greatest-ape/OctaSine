4b7cf5f7d3c8e7a9494a7eaadb77ee637620b879
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-12-02 16:53:33 +0100

    GUI: work on text and tick marks

--- octasine/src/gui/interface/widgets.rs
+++ octasine/src/gui/interface/widgets.rs
@@ -1,19 +1,23 @@
 use std::sync::Arc;
 
 use iced_baseview::{
-    Container, Column, Element, Text,
+    Container, Column, Element, Text, Length, Align
 };
 use iced_audio::{
-    knob, Normal, NormalParam
+    knob, Normal, NormalParam, text_marks, tick_marks
 };
 
 use crate::SyncOnlyState;
+use crate::constants::{MASTER_FREQUENCY_STEPS};
+
 use super::{ParameterWidget, Message};
 
 
 #[derive(Debug, Clone)]
 pub struct OctaSineKnob {
     knob_state: knob::State,
+    text_marks: Option<text_marks::Group>,
+    tick_marks: Option<tick_marks::Group>,
     title: String,
     parameter_index: usize,
 }
@@ -23,7 +27,9 @@ impl OctaSineKnob {
     pub fn new(
         sync_only: &Arc<SyncOnlyState>,
         title: String,
-        parameter_index: usize
+        parameter_index: usize,
+        text_marks: Option<text_marks::Group>,
+        tick_marks: Option<tick_marks::Group>,
     ) -> Self {
         let value = Normal::new(sync_only.presets.get_parameter_value_float(
             parameter_index
@@ -36,36 +42,85 @@ impl OctaSineKnob {
         
         Self {
             knob_state: knob::State::new(normal_param),
+            text_marks,
+            tick_marks,
             title,
             parameter_index
         }
+    }
+
+    pub fn master_volume(
+        sync_only: &Arc<SyncOnlyState>,
+    ) -> Self {
+        let parameter_index = 0;
+        let text_marks = text_marks_from_min_max_center(
+            &sync_only,
+            parameter_index
+        );
+        let tick_marks = tick_marks::Group::min_max_and_center(
+            tick_marks::Tier::One,
+            tick_marks::Tier::One,
+        );
+
+        Self::new(
+            &sync_only,
+            "Master\nvolume".to_string(),
+            parameter_index,
+            Some(text_marks),
+            Some(tick_marks),
+        )
+    }
+
+    pub fn master_frequency(
+        sync_only: &Arc<SyncOnlyState>,
+    ) -> Self {
+        let parameter_index = 1;
+        let text_marks = text_marks_from_steps(&MASTER_FREQUENCY_STEPS);
+        let tick_marks = tick_marks_from_steps(&MASTER_FREQUENCY_STEPS);
+
+        Self::new(
+            &sync_only,
+            "Master\nfrequency".to_string(),
+            parameter_index,
+            Some(text_marks),
+            Some(tick_marks),
+        )
     }
 }
 
 
 impl ParameterWidget for OctaSineKnob {
     fn view(&mut self, sync_only: &Arc<SyncOnlyState>) -> Element<Message> {
-        let parameter_index = self.parameter_index;
         let title = Text::new(self.title.clone()).size(12);
-
-        let value = sync_only.presets.format_parameter_value(
-            parameter_index,
+        let value = format_value(
+            sync_only,
+            self.parameter_index,
             self.knob_state.normal_param.value.as_f32() as f64
         );
         let value = Text::new(value).size(12);
 
-        let knob = knob::Knob::new(
+        let parameter_index = self.parameter_index;
+
+        let mut knob = knob::Knob::new(
             &mut self.knob_state,
             move |value| Message::ParameterChange(parameter_index, value),
-        );
+        )
+            .size(Length::from(Length::Units(31)));
+        
+        if let Some(text_marks) = self.text_marks.as_ref() {
+            knob = knob.text_marks(text_marks);
+        }
+        if let Some(tick_marks) = self.tick_marks.as_ref() {
+            knob = knob.tick_marks(tick_marks);
+        }
 
         let column = Column::new()
-            .push(Container::new(title).padding(4))
-            .push(Container::new(knob).padding(4))
-            .push(Container::new(value).padding(4));
+            .push(Container::new(title).padding(16))
+            .push(Container::new(knob).padding(16))
+            .push(Container::new(value).padding(16));
         
         Container::new(column)
-            .padding(4)
+            .padding(16)
             .into()
     }
 
@@ -74,4 +129,63 @@ impl ParameterWidget for OctaSineKnob {
             self.knob_state.normal_param.value = Normal::new(value as f32);
         }
     }
+}
+
+
+fn format_value(
+    sync_only: &Arc<SyncOnlyState>,
+    parameter_index: usize,
+    value: f64
+) -> String {
+    sync_only.presets.format_parameter_value(parameter_index, value)
+}
+
+
+fn text_marks_from_steps(steps: &[f64]) -> text_marks::Group {
+    let mut text_marks = Vec::with_capacity(steps.len());
+
+    let len = (steps.len() - 1) as f32;
+
+    for (index, value) in steps.iter().enumerate(){
+        let normal = Normal::new(index as f32 / len);
+        let text = format!("{:}", value);
+
+        text_marks.push((normal, text));
+    }
+
+    text_marks::Group::from(text_marks)
+}
+
+
+fn tick_marks_from_steps(steps: &[f64]) -> tick_marks::Group {
+    let mut tick_marks = Vec::with_capacity(steps.len());
+
+    let len = (steps.len() - 1) as f32;
+
+    for (index, value) in steps.iter().enumerate(){
+        let normal = Normal::new(index as f32 / len);
+
+        let tier = if index == 0 || index == steps.len() - 1 {
+            tick_marks::Tier::One
+        } else {
+            tick_marks::Tier::Two
+        };
+
+        tick_marks.push((normal, tier));
+    }
+
+    tick_marks::Group::from(tick_marks)
+}
+
+
+
+fn text_marks_from_min_max_center(
+    sync_only: &Arc<SyncOnlyState>,
+    parameter_index: usize,
+) -> text_marks::Group {
+    let min = format_value(sync_only, parameter_index, 0.0);
+    let max = format_value(sync_only, parameter_index, 1.0);
+    let center = format_value(sync_only, parameter_index, 0.5);
+
+    text_marks::Group::min_max_and_center(&min, &max, &center)
 }
