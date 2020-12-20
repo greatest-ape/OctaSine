98b19e81a68810e26cb55e58fc82457bc2abe480
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-12-20 01:18:51 +0100

    More work on simpler presets

---
+++ octasine/src/presets/parameters.rs
@@ -0,0 +1,120 @@
+use vst2_helpers::utils::atomic_double::AtomicPositiveDouble;
+use vst2_helpers::processing_parameters::utils::*;
+
+use crate::constants::*;
+
+
+
+trait ProcessingValueConversion {
+    fn from_sync(value: f64) -> Self;
+    fn to_sync(self) -> f64;
+    fn format(self) -> String;
+    fn format_sync(value: f64) -> String;
+}
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct MasterVolume(f64);
+
+
+impl ProcessingValueConversion for MasterVolume {
+    fn from_sync(value: f64) -> Self {
+        Self(value * 2.0)
+    }
+    fn to_sync(self) -> f64 {
+        self.0 / 2.0
+    }
+    fn format(self) -> String {
+        format!("{:.2}", 20.0 * self.0.log10())
+    }
+    fn format_sync(value: f64) -> String {
+        Self::from_sync(value).format()
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct MasterFrequency(f64);
+
+
+impl ProcessingValueConversion for MasterFrequency {
+    fn from_sync(sync: f64) -> Self {
+        Self(map_parameter_value_to_value_with_steps(
+            &MASTER_FREQUENCY_STEPS,
+            sync
+        ))
+    }
+    fn to_sync(self) -> f64 {
+        map_value_to_parameter_value_with_steps(&MASTER_FREQUENCY_STEPS, self.0)
+    }
+    fn format(self) -> String {
+        format!("{:.02}", self.0)
+    }
+    fn format_sync(value: f64) -> String {
+        Self::from_sync(value).format()
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+pub enum ProcessingValue {
+    MasterVolume(MasterVolume),
+    MasterFrequency(MasterFrequency),
+}
+
+
+impl ProcessingValue {
+}
+
+
+pub struct SyncParameter {
+    pub value: AtomicPositiveDouble,
+    pub name: String,
+    pub unit_from_value: fn(f64) -> String,
+    pub value_from_text: fn(String) -> Option<f64>,
+    pub to_processing: fn(f64) -> ProcessingValue,
+    pub format: fn(f64) -> String,
+}
+
+
+impl SyncParameter {
+    pub fn set_from_text(&self, text: String) -> bool {
+        if let Some(value) = (self.value_from_text)(text){
+            self.value.set(value);
+
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn master_volume() -> Self {
+        let value = MasterVolume(DEFAULT_MASTER_VOLUME).to_sync();
+
+        Self {
+            value: AtomicPositiveDouble::new(value),
+            name: "Master volume".to_string(),
+            unit_from_value: |_| "dB".to_string(),
+            value_from_text: |v| None,
+            to_processing: |v| ProcessingValue::MasterVolume(
+                MasterVolume::from_sync(v)
+            ),
+            format: |v| MasterVolume::from_sync(v).format(),
+        }
+    }
+
+    pub fn master_frequency() -> Self {
+        let value = MasterFrequency(DEFAULT_MASTER_FREQUENCY).to_sync();
+
+        Self {
+            value: AtomicPositiveDouble::new(value),
+            name: "Master frequency".to_string(),
+            unit_from_value: |_| "Hz".to_string(),
+            value_from_text: |v| None,
+            to_processing: |v| ProcessingValue::MasterFrequency(
+                MasterFrequency::from_sync(v)
+            ),
+            format: |v| MasterFrequency::from_sync(v).format(),
+        }
+    }
+}
