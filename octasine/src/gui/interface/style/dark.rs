7f267ee48bedae32b2764d222e56b77efdc5431b
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-06-20 21:05:51 +0200

    gui: remove unused stylesheets

--- octasine/src/gui/interface/style/dark.rs
+++ octasine/src/gui/interface/style/dark.rs
@@ -1,18 +1,11 @@
 use iced_baseview::{
-    button, checkbox, container, pick_list, progress_bar, radio, rule, scrollable, slider,
-    text_input, Background, Color,
+    button, container, pick_list, radio, Background, Color,
 };
 
 const SURFACE: Color = Color::from_rgb(
     0x40 as f32 / 255.0,
     0x44 as f32 / 255.0,
     0x4B as f32 / 255.0,
-);
-
-const ACCENT: Color = Color::from_rgb(
-    0x6F as f32 / 255.0,
-    0xFF as f32 / 255.0,
-    0xE9 as f32 / 255.0,
 );
 
 const ACTIVE: Color = Color::from_rgb(
@@ -59,47 +52,6 @@ impl radio::StyleSheet for Radio {
     }
 }
 
-pub struct TextInput;
-
-impl text_input::StyleSheet for TextInput {
-    fn active(&self) -> text_input::Style {
-        text_input::Style {
-            background: SURFACE.into(),
-            border_radius: 2.0,
-            border_width: 0.0,
-            border_color: Color::TRANSPARENT,
-        }
-    }
-
-    fn focused(&self) -> text_input::Style {
-        text_input::Style {
-            border_width: 1.0,
-            border_color: ACCENT,
-            ..self.active()
-        }
-    }
-
-    fn hovered(&self) -> text_input::Style {
-        text_input::Style {
-            border_width: 1.0,
-            border_color: Color { a: 0.3, ..ACCENT },
-            ..self.focused()
-        }
-    }
-
-    fn placeholder_color(&self) -> Color {
-        Color::from_rgb(0.4, 0.4, 0.4)
-    }
-
-    fn value_color(&self) -> Color {
-        Color::WHITE
-    }
-
-    fn selection_color(&self) -> Color {
-        ACTIVE
-    }
-}
-
 pub struct Button;
 
 impl button::StyleSheet for Button {
@@ -125,140 +77,6 @@ impl button::StyleSheet for Button {
             border_width: 1.0,
             border_color: Color::WHITE,
             ..self.hovered()
-        }
-    }
-}
-
-pub struct Scrollable;
-
-impl scrollable::StyleSheet for Scrollable {
-    fn active(&self) -> scrollable::Scrollbar {
-        scrollable::Scrollbar {
-            background: SURFACE.into(),
-            border_radius: 2.0,
-            border_width: 0.0,
-            border_color: Color::TRANSPARENT,
-            scroller: scrollable::Scroller {
-                color: ACTIVE,
-                border_radius: 2.0,
-                border_width: 0.0,
-                border_color: Color::TRANSPARENT,
-            },
-        }
-    }
-
-    fn hovered(&self) -> scrollable::Scrollbar {
-        let active = self.active();
-
-        scrollable::Scrollbar {
-            background: Color { a: 0.5, ..SURFACE }.into(),
-            scroller: scrollable::Scroller {
-                color: HOVERED,
-                ..active.scroller
-            },
-            ..active
-        }
-    }
-
-    fn dragging(&self) -> scrollable::Scrollbar {
-        let hovered = self.hovered();
-
-        scrollable::Scrollbar {
-            scroller: scrollable::Scroller {
-                color: Color::from_rgb(0.85, 0.85, 0.85),
-                ..hovered.scroller
-            },
-            ..hovered
-        }
-    }
-}
-
-pub struct Slider;
-
-impl slider::StyleSheet for Slider {
-    fn active(&self) -> slider::Style {
-        slider::Style {
-            rail_colors: (ACTIVE, Color { a: 0.1, ..ACTIVE }),
-            handle: slider::Handle {
-                shape: slider::HandleShape::Circle { radius: 9.0 },
-                color: ACTIVE,
-                border_width: 0.0,
-                border_color: Color::TRANSPARENT,
-            },
-        }
-    }
-
-    fn hovered(&self) -> slider::Style {
-        let active = self.active();
-
-        slider::Style {
-            handle: slider::Handle {
-                color: HOVERED,
-                ..active.handle
-            },
-            ..active
-        }
-    }
-
-    fn dragging(&self) -> slider::Style {
-        let active = self.active();
-
-        slider::Style {
-            handle: slider::Handle {
-                color: Color::from_rgb(0.85, 0.85, 0.85),
-                ..active.handle
-            },
-            ..active
-        }
-    }
-}
-
-pub struct ProgressBar;
-
-impl progress_bar::StyleSheet for ProgressBar {
-    fn style(&self) -> progress_bar::Style {
-        progress_bar::Style {
-            background: SURFACE.into(),
-            bar: ACTIVE.into(),
-            border_radius: 10.0,
-        }
-    }
-}
-
-pub struct Checkbox;
-
-impl checkbox::StyleSheet for Checkbox {
-    fn active(&self, is_checked: bool) -> checkbox::Style {
-        checkbox::Style {
-            background: if is_checked { ACTIVE } else { SURFACE }.into(),
-            checkmark_color: Color::WHITE,
-            border_radius: 2.0,
-            border_width: 1.0,
-            border_color: ACTIVE,
-        }
-    }
-
-    fn hovered(&self, is_checked: bool) -> checkbox::Style {
-        checkbox::Style {
-            background: Color {
-                a: 0.8,
-                ..if is_checked { ACTIVE } else { SURFACE }
-            }
-            .into(),
-            ..self.active(is_checked)
-        }
-    }
-}
-
-pub struct Rule;
-
-impl rule::StyleSheet for Rule {
-    fn style(&self) -> rule::Style {
-        rule::Style {
-            color: SURFACE,
-            width: 2,
-            radius: 1.0,
-            fill_mode: rule::FillMode::Padded(15),
         }
     }
 }
@@ -306,13 +124,6 @@ pub(super) mod knob {
         a: 0.65,
     };
 
-    pub const TEXT_MARK: Color = Color {
-        r: 0.26,
-        g: 0.26,
-        b: 0.26,
-        a: 0.93,
-    };
-
     pub const KNOB_BACK_HOVER: Color = Color::from_rgb(0.96, 0.96, 0.96);
 
     const ACTIVE_CIRCLE_STYLE: CircleStyle = CircleStyle {
@@ -342,14 +153,6 @@ pub(super) mod knob {
         }
         fn dragging(&self) -> iced_audio::knob::Style {
             self.hovered()
-        }
-        fn text_marks_style(&self) -> Option<TextMarksStyle> {
-            Some(TextMarksStyle {
-                style: iced_audio::text_marks::Style::default(),
-                offset: 14.0,
-                h_char_offset: 3.0,
-                v_offset: -0.75,
-            })
         }
         fn tick_marks_style(&self) -> Option<TickMarksStyle> {
             Some(TickMarksStyle {
--- octasine/src/gui/interface/style/mod.rs
+++ octasine/src/gui/interface/style/mod.rs
@@ -1,10 +1,7 @@
 mod dark;
 mod light;
 
-use iced_baseview::{
-    button, checkbox, container, pick_list, progress_bar, radio, rule, scrollable, slider,
-    text_input,
-};
+use iced_baseview::{button, container, pick_list, radio};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Theme {
@@ -40,65 +37,11 @@ impl From<Theme> for Box<dyn radio::StyleSheet> {
     }
 }
 
-impl From<Theme> for Box<dyn text_input::StyleSheet> {
-    fn from(theme: Theme) -> Self {
-        match theme {
-            Theme::Light => Default::default(),
-            Theme::Dark => dark::TextInput.into(),
-        }
-    }
-}
-
 impl From<Theme> for Box<dyn button::StyleSheet> {
     fn from(theme: Theme) -> Self {
         match theme {
             Theme::Light => light::Button.into(),
             Theme::Dark => dark::Button.into(),
-        }
-    }
-}
-
-impl From<Theme> for Box<dyn scrollable::StyleSheet> {
-    fn from(theme: Theme) -> Self {
-        match theme {
-            Theme::Light => Default::default(),
-            Theme::Dark => dark::Scrollable.into(),
-        }
-    }
-}
-
-impl From<Theme> for Box<dyn slider::StyleSheet> {
-    fn from(theme: Theme) -> Self {
-        match theme {
-            Theme::Light => Default::default(),
-            Theme::Dark => dark::Slider.into(),
-        }
-    }
-}
-
-impl From<Theme> for Box<dyn progress_bar::StyleSheet> {
-    fn from(theme: Theme) -> Self {
-        match theme {
-            Theme::Light => Default::default(),
-            Theme::Dark => dark::ProgressBar.into(),
-        }
-    }
-}
-
-impl From<Theme> for Box<dyn checkbox::StyleSheet> {
-    fn from(theme: Theme) -> Self {
-        match theme {
-            Theme::Light => Default::default(),
-            Theme::Dark => dark::Checkbox.into(),
-        }
-    }
-}
-
-impl From<Theme> for Box<dyn rule::StyleSheet> {
-    fn from(theme: Theme) -> Self {
-        match theme {
-            Theme::Light => Default::default(),
-            Theme::Dark => dark::Rule.into(),
         }
     }
 }
