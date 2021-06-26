9d8721b9f645eef77b50305e5ef00abdd357bf98
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-06-26 12:57:51 +0200

    GUI: clean up

--- octasine/src/gui/interface/style/light.rs
+++ octasine/src/gui/interface/style/light.rs
@@ -1,28 +1,6 @@
-use iced_baseview::{button, pick_list, Background, Color, Vector};
+use iced_baseview::{pick_list, Background, Color};
 
 use super::{envelope, mod_matrix};
-
-pub struct Button;
-
-impl button::StyleSheet for Button {
-    fn active(&self) -> button::Style {
-        button::Style {
-            background: Color::from_rgb(0.11, 0.42, 0.87).into(),
-            border_radius: 12.0,
-            shadow_offset: Vector::new(1.0, 1.0),
-            text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
-            ..button::Style::default()
-        }
-    }
-
-    fn hovered(&self) -> button::Style {
-        button::Style {
-            text_color: Color::WHITE,
-            shadow_offset: Vector::new(1.0, 2.0),
-            ..self.active()
-        }
-    }
-}
 
 pub struct PickList;
 
