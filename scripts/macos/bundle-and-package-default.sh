9cd5945f5fd9ca7e5c7dde821de7f35ab04ce1d9
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-07-02 12:05:39 +0200

    Add GitHub action for creating release when pushing tag

--- scripts/macos/bundle-and-package-default.sh
+++ scripts/macos/bundle-and-package-default.sh
@@ -3,4 +3,4 @@
 set -e
 
 ./scripts/macos/bundle.sh "./target/release/liboctasine.dylib"
-./scripts/macos/package.sh "OctaSine.vst" "OctaSine-macOS-Intel"
+./scripts/unix/package.sh "OctaSine.vst" "OctaSine-macOS-Intel"
