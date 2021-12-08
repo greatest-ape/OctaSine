9addeb277a79ad8ae0e9b5f7af5780005cf8018d
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-12-08 01:48:09 +0100

    scripts/macos/build-simd-and-install.sh: add glow feature line

--- scripts/macos/build-simd-and-install.sh
+++ scripts/macos/build-simd-and-install.sh
@@ -3,6 +3,7 @@
 set -e
 
 cargo +nightly build --release --features "simd" -p octasine_vst2_plugin
+# cargo +nightly build --release --features "simd,glow" --no-default-features -p octasine_vst2_plugin
 
 ./scripts/macos/bundle.sh "./target/release/liboctasine.dylib"
 ./scripts/macos/install.sh "./tmp/OctaSine.vst"
