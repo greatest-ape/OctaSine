9d7c4dab84ca661cf81a066d6a678c595dafd4ab
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-12-08 01:59:32 +0100

    plugin: use Error log level, but include initial info logs

--- octasine/src/lib.rs
+++ octasine/src/lib.rs
@@ -116,7 +116,7 @@ impl OctaSine {
             .set_time_to_local(true)
             .build();
 
-        let _ = simplelog::WriteLogger::init(simplelog::LevelFilter::Warn, log_config, log_file);
+        let _ = simplelog::WriteLogger::init(simplelog::LevelFilter::Info, log_config, log_file);
 
         log_panics::init();
 
@@ -124,6 +124,8 @@ impl OctaSine {
 
         ::log::info!("OS: {}", ::os_info::get());
         ::log::info!("OctaSine build: {}", get_version_info());
+
+        ::log::set_max_level(simplelog::LevelFilter::Error);
     }
 
     fn time_per_sample(sample_rate: SampleRate) -> TimePerSample {
