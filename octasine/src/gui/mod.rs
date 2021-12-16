404da4a759631c69634d7c880b2d861207988414
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-12-17 00:45:26 +0100

    GUI: use scale factor 1.0 on Windows

--- octasine/src/gui/mod.rs
+++ octasine/src/gui/mod.rs
@@ -37,7 +37,12 @@ impl<H: GuiSyncHandle> Gui<H> {
         Settings {
             window: WindowOpenOptions {
                 size: Size::new(GUI_WIDTH as f64, GUI_HEIGHT as f64),
+                #[cfg(not(target_os = "windows"))]
                 scale: WindowScalePolicy::SystemScaleFactor,
+                // Windows currently needs scale factor 1.0, or GUI contents
+                // will be too large for window
+                #[cfg(target_os = "windows")]
+                scale: WindowScalePolicy::ScaleFactor(1.0),
                 title: PLUGIN_NAME.to_string(),
             },
             #[cfg(all(feature = "gui_glow", target_os = "windows"))]
