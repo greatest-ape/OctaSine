6af049117e04d58c7b3ba29e90c1931653366213
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-12-28 23:57:05 +0100

    audio gen: move VoiceData and SingleAccessLock to own file

--- octasine/src/gen/mod.rs
+++ octasine/src/gen/mod.rs
@@ -1,12 +1,6 @@
 mod lfo;
 pub mod simd;
-
-use std::cell::UnsafeCell;
-use std::ops::Deref;
-use std::ops::DerefMut;
-use std::sync::Arc;
-use std::sync::atomic::AtomicUsize;
-use std::sync::atomic::Ordering;
+mod voice_data;
 
 use duplicate::duplicate;
 use vst::buffer::AudioBuffer;
@@ -18,6 +12,7 @@ use crate::OctaSine;
 
 use lfo::*;
 use simd::*;
+use voice_data::*;
 
 enum RemainingSamples {
     FourOrMore,
@@ -37,61 +32,6 @@ impl RemainingSamples {
         } else {
             Self::Zero
         }
-    }
-}
-
-/// Lock allowing access only from a single thread at a time
-struct SingleAccessLock<T> {
-    inner: UnsafeCell<T>,
-    holders: Arc<AtomicUsize>,
-}
-
-impl<T> SingleAccessLock<T> {
-    fn new(contents: T) -> Self {
-        Self {
-            inner: UnsafeCell::new(contents),
-            holders: Arc::new(AtomicUsize::new(0)),
-        }
-    }
-
-    fn get_mut(&self) -> Option<SingleAccessLockGuard<T>> {
-        if let Ok(_) = self.holders.compare_exchange(0, 1, Ordering::SeqCst, Ordering::SeqCst) {
-            if let Some(reference) = unsafe { self.inner.get().as_mut() } {
-                return Some(SingleAccessLockGuard {
-                    reference,
-                    holders: &self.holders,
-                })
-            }
-        }
-
-        None
-    }
-}
-
-unsafe impl<T> Sync for SingleAccessLock<T> {}
-
-struct SingleAccessLockGuard<'a, T> {
-    reference: &'a mut T,
-    holders: &'a Arc<AtomicUsize>,
-}
-
-impl<'a, T> Deref for SingleAccessLockGuard<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.reference
-    }
-}
-
-impl<'a, T> DerefMut for SingleAccessLockGuard<'a, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.reference
-    }
-}
-
-impl<'a, T> Drop for SingleAccessLockGuard<'a, T> {
-    fn drop(&mut self) {
-        self.holders.fetch_sub(1, Ordering::SeqCst);
     }
 }
 
@@ -155,63 +95,6 @@ pub trait AudioGen {
 }
 
 #[derive(Debug)]
-pub struct VoiceData<const PD_WIDTH: usize, const SAMPLES: usize> {
-    active: bool,
-    operator_volumes: [[f64; PD_WIDTH]; 4],
-    operator_modulation_indices: [[f64; PD_WIDTH]; 4],
-    operator_feedbacks: [[f64; PD_WIDTH]; 4],
-    operator_additives: [[f64; PD_WIDTH]; 4],
-    operator_frequencies: [[f64; PD_WIDTH]; 4],
-    operator_pannings: [[f64; SAMPLES]; 4],
-    operator_constant_power_pannings: [[f64; PD_WIDTH]; 4],
-    operator_envelope_volumes: [[f64; PD_WIDTH]; 4],
-    operator_phases: [[f64; PD_WIDTH]; 4],
-    operator_wave_type: [crate::WaveType; 4],
-    operator_modulation_targets: [usize; 4],
-    volume_factors: [f64; 4],
-}
-
-impl Default for VoiceData<2, 1> {
-    fn default() -> Self {
-        Self {
-            active: false,
-            operator_volumes: Default::default(),
-            operator_modulation_indices: Default::default(),
-            operator_feedbacks: Default::default(),
-            operator_additives: Default::default(),
-            operator_frequencies: Default::default(),
-            operator_pannings: Default::default(),
-            operator_constant_power_pannings: Default::default(),
-            operator_envelope_volumes: Default::default(),
-            operator_phases: Default::default(),
-            operator_wave_type: Default::default(),
-            operator_modulation_targets: Default::default(),
-            volume_factors: Default::default(),
-        }
-    }
-}
-
-impl Default for VoiceData<4, 2> {
-    fn default() -> Self {
-        Self {
-            active: false,
-            operator_volumes: Default::default(),
-            operator_modulation_indices: Default::default(),
-            operator_feedbacks: Default::default(),
-            operator_additives: Default::default(),
-            operator_frequencies: Default::default(),
-            operator_pannings: Default::default(),
-            operator_constant_power_pannings: Default::default(),
-            operator_envelope_volumes: Default::default(),
-            operator_phases: Default::default(),
-            operator_wave_type: Default::default(),
-            operator_modulation_targets: Default::default(),
-            volume_factors: Default::default(),
-        }
-    }
-}
-
-#[derive(Debug)]
 pub enum Error {
     ConcurrentAccess,
 }
@@ -249,8 +132,8 @@ mod gen {
     type VoiceData = super::VoiceData<{S::PD_WIDTH}, {S::SAMPLES}>;
 
     #[feature_gate]
-    static VOICE_DATA: Lazy<SingleAccessLock<[VoiceData; 128]>> = Lazy::new(|| {
-        SingleAccessLock::new(array_init::array_init(|_| VoiceData::default()))
+    static VOICE_DATA: Lazy<super::voice_data::SingleAccessLock<[VoiceData; 128]>> = Lazy::new(|| {
+        super::voice_data::SingleAccessLock::new(array_init::array_init(|_| VoiceData::default()))
     });
 
     #[feature_gate]
---
+++ octasine/src/gen/voice_data.rs
@@ -0,0 +1,118 @@
+use std::cell::UnsafeCell;
+use std::ops::Deref;
+use std::ops::DerefMut;
+use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+
+/// Lock allowing access only from a single thread at a time
+pub struct SingleAccessLock<T> {
+    inner: UnsafeCell<T>,
+    holders: Arc<AtomicUsize>,
+}
+
+impl<T> SingleAccessLock<T> {
+    pub fn new(contents: T) -> Self {
+        Self {
+            inner: UnsafeCell::new(contents),
+            holders: Arc::new(AtomicUsize::new(0)),
+        }
+    }
+
+    pub fn get_mut(&self) -> Option<SingleAccessLockGuard<T>> {
+        if let Ok(_) = self.holders.compare_exchange(0, 1, Ordering::SeqCst, Ordering::SeqCst) {
+            if let Some(reference) = unsafe { self.inner.get().as_mut() } {
+                return Some(SingleAccessLockGuard {
+                    reference,
+                    holders: &self.holders,
+                })
+            }
+        }
+
+        None
+    }
+}
+
+unsafe impl<T> Sync for SingleAccessLock<T> {}
+
+pub struct SingleAccessLockGuard<'a, T> {
+    reference: &'a mut T,
+    holders: &'a Arc<AtomicUsize>,
+}
+
+impl<'a, T> Deref for SingleAccessLockGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.reference
+    }
+}
+
+impl<'a, T> DerefMut for SingleAccessLockGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.reference
+    }
+}
+
+impl<'a, T> Drop for SingleAccessLockGuard<'a, T> {
+    fn drop(&mut self) {
+        self.holders.fetch_sub(1, Ordering::SeqCst);
+    }
+}
+
+#[derive(Debug)]
+pub struct VoiceData<const PD_WIDTH: usize, const SAMPLES: usize> {
+    pub active: bool,
+    pub operator_volumes: [[f64; PD_WIDTH]; 4],
+    pub operator_modulation_indices: [[f64; PD_WIDTH]; 4],
+    pub operator_feedbacks: [[f64; PD_WIDTH]; 4],
+    pub operator_additives: [[f64; PD_WIDTH]; 4],
+    pub operator_frequencies: [[f64; PD_WIDTH]; 4],
+    pub operator_pannings: [[f64; SAMPLES]; 4],
+    pub operator_constant_power_pannings: [[f64; PD_WIDTH]; 4],
+    pub operator_envelope_volumes: [[f64; PD_WIDTH]; 4],
+    pub operator_phases: [[f64; PD_WIDTH]; 4],
+    pub operator_wave_type: [crate::WaveType; 4],
+    pub operator_modulation_targets: [usize; 4],
+    pub volume_factors: [f64; 4],
+}
+
+impl Default for VoiceData<2, 1> {
+    fn default() -> Self {
+        Self {
+            active: false,
+            operator_volumes: Default::default(),
+            operator_modulation_indices: Default::default(),
+            operator_feedbacks: Default::default(),
+            operator_additives: Default::default(),
+            operator_frequencies: Default::default(),
+            operator_pannings: Default::default(),
+            operator_constant_power_pannings: Default::default(),
+            operator_envelope_volumes: Default::default(),
+            operator_phases: Default::default(),
+            operator_wave_type: Default::default(),
+            operator_modulation_targets: Default::default(),
+            volume_factors: Default::default(),
+        }
+    }
+}
+
+impl Default for VoiceData<4, 2> {
+    fn default() -> Self {
+        Self {
+            active: false,
+            operator_volumes: Default::default(),
+            operator_modulation_indices: Default::default(),
+            operator_feedbacks: Default::default(),
+            operator_additives: Default::default(),
+            operator_frequencies: Default::default(),
+            operator_pannings: Default::default(),
+            operator_constant_power_pannings: Default::default(),
+            operator_envelope_volumes: Default::default(),
+            operator_phases: Default::default(),
+            operator_wave_type: Default::default(),
+            operator_modulation_targets: Default::default(),
+            volume_factors: Default::default(),
+        }
+    }
+}
