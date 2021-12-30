6dfef1a81eb4ee76fac00b40b67057126ca6e6d9
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-12-30 01:56:13 +0100

    audio gen: improve SingleAccessLock

--- octasine/src/gen/voice_data.rs
+++ octasine/src/gen/voice_data.rs
@@ -1,33 +1,33 @@
 use std::cell::UnsafeCell;
 use std::ops::Deref;
 use std::ops::DerefMut;
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
 
-/// Lock allowing access only from a single thread at a time
+/// Non-blocking lock allowing access only from a single thread at a time
 pub struct SingleAccessLock<T> {
-    inner: UnsafeCell<T>,
-    holders: Arc<AtomicUsize>,
+    contents: UnsafeCell<T>,
+    borrowed: Arc<AtomicBool>,
 }
 
 impl<T> SingleAccessLock<T> {
     pub fn new(contents: T) -> Self {
         Self {
-            inner: UnsafeCell::new(contents),
-            holders: Arc::new(AtomicUsize::new(0)),
+            contents: UnsafeCell::new(contents),
+            borrowed: Arc::new(AtomicBool::new(false)),
         }
     }
 
     pub fn get_mut(&self) -> Option<SingleAccessLockGuard<T>> {
-        if let Ok(_) = self
-            .holders
-            .compare_exchange(0, 1, Ordering::SeqCst, Ordering::SeqCst)
+        if let Ok(_) =
+            self.borrowed
+                .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
         {
-            if let Some(reference) = unsafe { self.inner.get().as_mut() } {
+            if let Some(reference) = unsafe { self.contents.get().as_mut() } {
                 return Some(SingleAccessLockGuard {
                     reference,
-                    holders: &self.holders,
+                    borrowed: &self.borrowed,
                 });
             }
         }
@@ -40,7 +40,7 @@ unsafe impl<T> Sync for SingleAccessLock<T> {}
 
 pub struct SingleAccessLockGuard<'a, T> {
     reference: &'a mut T,
-    holders: &'a Arc<AtomicUsize>,
+    borrowed: &'a Arc<AtomicBool>,
 }
 
 impl<'a, T> Deref for SingleAccessLockGuard<'a, T> {
@@ -59,7 +59,7 @@ impl<'a, T> DerefMut for SingleAccessLockGuard<'a, T> {
 
 impl<'a, T> Drop for SingleAccessLockGuard<'a, T> {
     fn drop(&mut self) {
-        self.holders.store(0, Ordering::SeqCst);
+        self.borrowed.store(false, Ordering::SeqCst);
     }
 }
 
