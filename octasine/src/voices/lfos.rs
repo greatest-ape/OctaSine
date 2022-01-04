b077bdc85e90b6c1f653f3d65344ecd72e0af2f9
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-01-04 20:12:26 +0100

    LFOs: use sample num interpolation, do each cycle and mid in some cases

--- octasine/src/voices/lfos.rs
+++ octasine/src/voices/lfos.rs
@@ -1,25 +1,22 @@
 use crate::common::*;
 
-const INTERPOLATION_TIME: f64 = 0.01;
+const INTERPOLATION_SAMPLES: usize = 128;
 
 #[derive(Debug, Clone)]
 enum LfoStage {
-    Restart {
-        value_before_restart: f64,
-        current_shape: Option<LfoShape>,
+    Interpolate {
+        from_value: f64,
+        samples_done: usize,
+        stop_afterwards: bool,
     },
-    Running {
-        current_shape: Option<LfoShape>,
-    },
-    Stopping {
-        value_before_stopping: f64,
-    },
-    Stopped
+    Running,
+    Stopped,
 }
 
 #[derive(Debug, Clone)]
 pub struct VoiceLfo {
     stage: LfoStage,
+    current_shape: Option<LfoShape>,
     phase: Phase,
     last_value: f64,
 }
@@ -28,6 +25,7 @@ impl Default for VoiceLfo {
     fn default() -> Self {
         Self {
             stage: LfoStage::Stopped,
+            current_shape: None,
             phase: Phase(0.0),
             last_value: 0.0,
         }
@@ -47,43 +45,65 @@ impl VoiceLfo {
             return;
         }
 
+        if self.current_shape.is_none() {
+            self.current_shape = Some(shape);
+        }
+
         let new_phase = self.phase.0 + frequency * (bpm.0 / 120.0) * time_per_sample.0;
 
         match self.stage {
-            LfoStage::Restart { current_shape, value_before_restart } => {
-                let shape = current_shape.unwrap_or(shape);
+            LfoStage::Interpolate {
+                from_value,
+                stop_afterwards,
+                mut samples_done,
+            } => {
+                samples_done += 1;
 
-                if new_phase > INTERPOLATION_TIME {
-                    self.stage = LfoStage::Running {
-                        current_shape: Some(shape)
-                    };
+                if samples_done == INTERPOLATION_SAMPLES {
+                    if stop_afterwards {
+                        self.stage = LfoStage::Stopped;
+                        self.last_value = 0.0;
+                    } else {
+                        self.stage = LfoStage::Running;
+                    }
                 } else {
-                    self.stage = LfoStage::Restart {
-                        current_shape: Some(shape),
-                        value_before_restart
+                    self.stage = LfoStage::Interpolate {
+                        from_value,
+                        samples_done,
+                        stop_afterwards,
                     }
                 }
 
                 self.phase.0 = new_phase;
-            },
-            LfoStage::Running { .. } => {
+            }
+            LfoStage::Running => {
                 if new_phase >= 1.0 {
+                    self.phase.0 = new_phase.fract();
+
                     if mode == LfoMode::Once {
                         self.request_stop();
                     } else {
-                        self.stage = LfoStage::Running {
-                            current_shape: Some(shape),
-                        };
+                        self.current_shape = Some(shape);
 
-                        self.phase.0 = new_phase.fract();
+                        self.stage = LfoStage::Interpolate {
+                            from_value: self.last_value,
+                            samples_done: 0,
+                            stop_afterwards: false,
+                        }
                     }
                 } else {
                     self.phase.0 = new_phase;
-                }
-            },
-            LfoStage::Stopping { .. } => {
-                if new_phase > INTERPOLATION_TIME {
-                    self.stage = LfoStage::Stopped;
+
+                    match self.current_shape.unwrap() {
+                        LfoShape::Square | LfoShape::ReverseSquare if self.phase.0 >= 0.5 => {
+                            self.stage = LfoStage::Interpolate {
+                                from_value: self.last_value,
+                                samples_done: 0,
+                                stop_afterwards: false,
+                            }
+                        }
+                        _ => {}
+                    }
                 }
             }
             LfoStage::Stopped => {
@@ -97,34 +117,28 @@ impl VoiceLfo {
             return 0.0;
         }
 
+        let shape = if let Some(shape) = self.current_shape {
+            shape
+        } else {
+            return 0.0;
+        };
+
         let value = match self.stage {
-            LfoStage::Restart { current_shape, value_before_restart } => {
-                let shape = if let Some(shape) = current_shape {
-                    shape
+            LfoStage::Interpolate {
+                from_value,
+                stop_afterwards,
+                samples_done,
+            } => {
+                let progress = samples_done as f64 / INTERPOLATION_SAMPLES as f64;
+
+                if stop_afterwards {
+                    from_value - from_value * progress
                 } else {
-                    return 0.0;
-                };
-
-                let value = Self::calculate_curve(shape, self.phase);
-
-                let interpolation_advancement = self.phase.0 / INTERPOLATION_TIME;
-
-                interpolation_advancement * value + (1.0 - interpolation_advancement) * value_before_restart
-            },
-            LfoStage::Running { current_shape } => {
-                let shape = if let Some(shape) = current_shape {
-                    shape
-                } else {
-                    return 0.0;
-                };
-
-                Self::calculate_curve(shape, self.phase)
+                    progress * Self::calculate_curve(shape, self.phase)
+                        + (1.0 - progress) * from_value
+                }
             }
-            LfoStage::Stopping { value_before_stopping } => {
-                let interpolation_advancement = self.phase.0 / INTERPOLATION_TIME;
-
-                value_before_stopping - value_before_stopping * interpolation_advancement
-            }
+            LfoStage::Running => Self::calculate_curve(shape, self.phase),
             LfoStage::Stopped => {
                 unreachable!()
             }
@@ -137,9 +151,9 @@ impl VoiceLfo {
 
     fn calculate_curve(shape: LfoShape, phase: Phase) -> f64 {
         match shape {
-            LfoShape::Saw => triangle(phase, Phase(0.9)),
-            LfoShape::ReverseSaw => triangle(phase, Phase(0.1)),
-            LfoShape::Triangle => triangle(phase, Phase(0.5)),
+            LfoShape::Saw => saw(phase),
+            LfoShape::ReverseSaw => reverse_saw(phase),
+            LfoShape::Triangle => triangle(phase),
             LfoShape::Square => square(phase),
             LfoShape::ReverseSquare => rev_square(phase),
         }
@@ -147,64 +161,58 @@ impl VoiceLfo {
 
     pub fn restart(&mut self) {
         self.phase = Phase(0.0);
+        self.current_shape = None;
 
-        match self.stage {
-            LfoStage::Restart { .. } | LfoStage::Running { .. } | LfoStage::Stopping { .. } => {
-                self.stage = LfoStage::Restart {
-                    current_shape: None,
-                    value_before_restart: self.last_value,
-                }
-            }
-            LfoStage::Stopped => {
-                self.stage = LfoStage::Running { current_shape: None };
-            }
-        }
+        self.stage = match self.stage {
+            LfoStage::Stopped => LfoStage::Running,
+            _ => LfoStage::Interpolate {
+                from_value: self.last_value,
+                samples_done: 0,
+                stop_afterwards: false,
+            },
+        };
     }
 
     pub fn request_stop(&mut self) {
-        self.stage = LfoStage::Stopping {
-            value_before_stopping: self.last_value,
+        self.stage = LfoStage::Interpolate {
+            from_value: self.last_value,
+            samples_done: 0,
+            stop_afterwards: true,
         };
         self.phase.0 = 0.0;
     }
 }
 
-fn triangle(phase: Phase, peak: Phase) -> f64 {
-    if phase.0 <= peak.0 {
-        phase.0 / peak.0
+fn triangle(phase: Phase) -> f64 {
+    let peak = 0.5;
+
+    if phase.0 <= peak {
+        phase.0 / peak
     } else {
-        1.0 - (phase.0 - peak.0) / (1.0 - peak.0)
+        1.0 - (phase.0 - peak) / (1.0 - peak)
     }
 }
 
-fn square(phase: Phase) -> f64 {
-    let peak_start = 0.1;
-    let peak_end = 0.5;
-    let base_start = 0.6;
+fn saw(phase: Phase) -> f64 {
+    phase.0
+}
 
-    if phase.0 <= peak_start {
-        phase.0 / peak_start
-    } else if phase.0 <= peak_end {
+fn reverse_saw(phase: Phase) -> f64 {
+    1.0 - phase.0
+}
+
+fn square(phase: Phase) -> f64 {
+    if phase.0 <= 0.5 {
         1.0
-    } else if phase.0 <= base_start {
-        1.0 - (phase.0 - peak_end) / (base_start - peak_end)
     } else {
         0.0
     }
 }
 
 fn rev_square(phase: Phase) -> f64 {
-    let base_end = 0.4;
-    let peak_start = 0.5;
-    let peak_end = 0.9;
-
-    if phase.0 <= base_end {
+    if phase.0 <= 0.5 {
         0.0
-    } else if phase.0 <= peak_start {
-        (phase.0 - base_end) / (peak_start - base_end)
-    } else if phase.0 <= peak_end {
-        1.0
     } else {
-        1.0 - (phase.0 - peak_end) / (1.0 - peak_end)
+        1.0
     }
 }
