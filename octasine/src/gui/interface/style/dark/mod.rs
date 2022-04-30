f9e8fc373df8c755da8c41de803ef7b81841f2e5
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-04-30 17:14:58 +0200

    WIP: gui

--- octasine/src/gui/interface/style/dark/mod.rs
+++ octasine/src/gui/interface/style/dark/mod.rs
@@ -48,12 +48,12 @@ pub struct Checkbox;
 impl checkbox::StyleSheet for Checkbox {
     fn active(&self, _is_checked: bool) -> checkbox::Style {
         checkbox::Style {
-            background: SURFACE.into(),
-            checkmark_color: TEXT_FG,
+            background: Color::TRANSPARENT.into(),
+            checkmark_color: BLUE,
             text_color: Some(TEXT_FG),
             border_width: 1.0,
             border_color: TEXT_FG,
-            border_radius: 5.0,
+            border_radius: 3.0,
         }
     }
 
@@ -167,10 +167,10 @@ pub struct LfoShapePicker;
 impl wave_picker::StyleSheet for LfoShapePicker {
     fn active(&self) -> wave_picker::Style {
         wave_picker::Style {
-            background_color: SURFACE,
-            border_color_active: TEXT_FG,
+            background_color: Color::TRANSPARENT.into(),
+            border_color_active: GRAY_700,
             border_color_hovered: HOVERED,
-            middle_line_color: Color::from_rgb(0.3, 0.3, 0.3),
+            middle_line_color: GRAY_400,
             shape_line_color_active: TEXT_FG,
             shape_line_color_hovered: HOVERED,
         }
@@ -182,21 +182,21 @@ pub struct MuteButton;
 impl mute_button::StyleSheet for MuteButton {
     fn volume_on(&self) -> mute_button::Style {
         mute_button::Style {
-            background_color: SURFACE,
-            border_color: TEXT_BG,
+            background_color: Color::TRANSPARENT,
+            border_color: TEXT_FG,
             text_color: TEXT_FG,
         }
     }
     fn volume_off(&self) -> mute_button::Style {
         mute_button::Style {
-            background_color: SURFACE,
+            background_color: Color::TRANSPARENT,
             border_color: RED,
             text_color: RED,
         }
     }
     fn hovered(&self) -> mute_button::Style {
         mute_button::Style {
-            background_color: SURFACE,
+            background_color: Color::TRANSPARENT,
             border_color: HOVERED,
             text_color: HOVERED,
         }
