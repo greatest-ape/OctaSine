e3095f2f99eb68275f31e1b113a0958a004b9cd2
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-05-11 16:59:37 +0200

    Add InterpolationDuration::samples() method and tests

--- octasine/src/audio/common.rs
+++ octasine/src/audio/common.rs
@@ -1,9 +1,14 @@
+use crate::common::SampleRate;
+
 #[derive(Debug, Copy, Clone)]
 pub struct InterpolationDuration(f64);
 
 impl InterpolationDuration {
     pub fn get(&self) -> f64 {
         self.0
+    }
+    pub fn samples(&self, sample_rate: SampleRate) -> usize {
+        (self.0 * sample_rate.0).round() as usize
     }
     /// 0.52 ms. 22.96875 samples with 44.1 kHz, 25 with 48 kHz
     pub fn fast() -> Self {
@@ -19,8 +24,16 @@ impl InterpolationDuration {
     }
 }
 
-impl Default for InterpolationDuration {
-    fn default() -> Self {
-        Self::slow()
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interpolation_duration_samples() {
+        let sample_rate = SampleRate(44100.0);
+
+        assert_eq!(InterpolationDuration::slow().samples(sample_rate), 147);
+        assert_eq!(InterpolationDuration::medium().samples(sample_rate), 46);
+        assert_eq!(InterpolationDuration::fast().samples(sample_rate), 23);
     }
 }
