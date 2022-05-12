cd506045aff2025d518dfbf118f416212987d7ac
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-05-12 10:46:10 +0200

    InterpolationDuration: add constructors and tests

--- octasine/src/audio/common.rs
+++ octasine/src/audio/common.rs
@@ -3,6 +3,7 @@ use crate::common::SampleRate;
 #[derive(Debug, Copy, Clone)]
 pub struct InterpolationDuration(f64);
 
+#[allow(dead_code)]
 impl InterpolationDuration {
     pub fn samples(&self, sample_rate: SampleRate) -> usize {
         (self.0 * sample_rate.0).round() as usize
@@ -19,6 +20,24 @@ impl InterpolationDuration {
 
         Self(DURATION)
     }
+    /// 33.33 ms. 1470 samples with 44.1 Hz, 1600 with 48 kHz
+    pub const fn approx_30ms() -> Self {
+        const DURATION: f64 = 1.0 / 30.0;
+
+        Self(DURATION)
+    }
+    pub const fn exactly_10ms() -> Self {
+        Self(0.01)
+    }
+    pub const fn exactly_20ms() -> Self {
+        Self(0.02)
+    }
+    pub const fn exactly_50ms() -> Self {
+        Self(0.05)
+    }
+    pub const fn exactly_100ms() -> Self {
+        Self(0.1)
+    }
 }
 
 #[cfg(test)]
@@ -30,9 +49,15 @@ mod tests {
         use InterpolationDuration as D;
 
         assert_eq!(D::approx_1ms().samples(SampleRate(44100.0)), 46);
-        assert_eq!(D::approx_3ms().samples(SampleRate(44100.0)), 147);
-
         assert_eq!(D::approx_1ms().samples(SampleRate(48000.0)), 50);
+
+        assert_eq!(D::approx_3ms().samples(SampleRate(44100.0)), 147);
         assert_eq!(D::approx_3ms().samples(SampleRate(48000.0)), 160);
+
+        assert_eq!(D::approx_30ms().samples(SampleRate(44100.0)), 1470);
+        assert_eq!(D::approx_30ms().samples(SampleRate(48000.0)), 1600);
+
+        assert_eq!(D::exactly_10ms().samples(SampleRate(44100.0)), 441);
+        assert_eq!(D::exactly_10ms().samples(SampleRate(48000.0)), 480);
     }
 }
