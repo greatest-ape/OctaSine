cf1de416674c09c374596b3b23fa8d961cb38c01
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-05-15 20:26:28 +0200

    bench_process: improve code, fix delta_frames bug risk

--- bench_process/src/main.rs
+++ bench_process/src/main.rs
@@ -64,13 +64,12 @@ fn main() {
         "59 ee 27 9b d0 2f 9c 08 ",
     );
 
-    #[allow(unused_variables, unused_mut)]
-    let mut all_sleef_hashes_match = true;
-
     #[cfg(feature = "simd")]
     {
         // Don't forget trailing space
         let hash = "59 ee 27 9b d0 2f 9c 08 ";
+
+        let mut all_sleef_hashes_match = true;
 
         {
             let (success, r) =
@@ -95,17 +94,25 @@ fn main() {
 
             println!("Speed compared to std fallback: {}x", fallback_std / r);
         }
-    }
 
-    if all_sleef_hashes_match {
-        println!("\n{}", "All sleef output hashes matched".green());
-    } else {
-        println!("\n{}", "Sleef output hashes didn't match".red());
+        if all_sleef_hashes_match {
+            println!(
+                "\n{}",
+                "All sleef output hashes matched reference hash".green()
+            );
+        } else {
+            println!(
+                "\n{}",
+                "Sleef output hashes didn't match reference hash".red()
+            );
+        }
     }
 }
 
 fn benchmark<A: AudioGen + Simd>(name: &str, expected_hash: &str) -> (bool, f32) {
-    let mut octasine = OctaSine::default();
+    const BUFFER_LEN: usize = 256;
+    const BUFFER_ITERATIONS: usize = 1024 * 8;
+    const NUM_VOICES: usize = 4;
 
     let envelope_duration_parameters: Vec<Parameter> = (0..4)
         .map(|i| {
@@ -122,14 +129,41 @@ fn benchmark<A: AudioGen + Simd>(name: &str, expected_hash: &str) -> (bool, f32)
         .map(|i| Parameter::Operator(i, OperatorParameter::WaveType))
         .collect();
 
-    const SIZE: usize = 256;
+    let parameters_to_automate: Vec<Parameter> = PARAMETERS
+        .iter()
+        .copied()
+        .filter(|p| !envelope_duration_parameters.contains(p) && !wave_type_parameters.contains(p))
+        .collect();
 
-    let mut lefts = vec![0.0f32; SIZE];
-    let mut rights = vec![0.0f32; SIZE];
+    let key_on_events: Vec<MidiEvent> = (0..NUM_VOICES)
+        .map(|i| MidiEvent {
+            data: [144, i as u8, 100],
+            delta_frames: (i % A::SAMPLES) as i32,
+            live: false,
+            note_length: None,
+            note_offset: None,
+            detune: 0,
+            note_off_velocity: 0,
+        })
+        .collect();
 
-    let mut results = Sha256::new();
+    let key_off_events: Vec<MidiEvent> = (0..NUM_VOICES)
+        .map(|i| MidiEvent {
+            data: [128, i as u8, 0],
+            delta_frames: (i % A::SAMPLES) as i32,
+            live: false,
+            note_length: None,
+            note_offset: None,
+            detune: 0,
+            note_off_velocity: 0,
+        })
+        .collect();
 
-    let iterations = 5_000;
+    let mut lefts = [0.0f32; BUFFER_LEN];
+    let mut rights = [0.0f32; BUFFER_LEN];
+
+    let mut octasine = OctaSine::default();
+    let mut output_hasher = Sha256::new();
 
     for p in envelope_duration_parameters.iter() {
         octasine.sync.set_parameter(p.to_index() as i32, 0.1);
@@ -140,48 +174,22 @@ fn benchmark<A: AudioGen + Simd>(name: &str, expected_hash: &str) -> (bool, f32)
 
     let now = Instant::now();
 
-    let key_on_events: Vec<MidiEvent> = (0..=3usize)
-        .map(|i| MidiEvent {
-            data: [144, 100 + i as u8, 100],
-            delta_frames: i as i32,
-            live: false,
-            note_length: None,
-            note_offset: None,
-            detune: 0,
-            note_off_velocity: 0,
-        })
-        .collect();
-
-    let key_off_events: Vec<MidiEvent> = (0..=3usize)
-        .map(|i| MidiEvent {
-            data: [128, 100 + i as u8, 0],
-            delta_frames: i as i32,
-            live: false,
-            note_length: None,
-            note_offset: None,
-            detune: 0,
-            note_off_velocity: 0,
-        })
-        .collect();
-
-    for i in 0..iterations {
-        if i % 1024 == 0 {
-            octasine
-                .audio
-                .enqueue_midi_events(key_on_events.iter().copied());
-        } else if i % 1024 == 512 {
-            octasine
-                .audio
-                .enqueue_midi_events(key_off_events.iter().copied());
+    for i in 0..BUFFER_ITERATIONS {
+        match i % 1024 {
+            0 => {
+                octasine
+                    .audio
+                    .enqueue_midi_events(key_on_events.iter().copied());
+            }
+            512 => {
+                octasine
+                    .audio
+                    .enqueue_midi_events(key_off_events.iter().copied());
+            }
+            _ => {}
         }
 
-        for parameter in PARAMETERS.iter() {
-            if envelope_duration_parameters.contains(parameter)
-                || wave_type_parameters.contains(parameter)
-            {
-                continue;
-            }
-
+        for parameter in parameters_to_automate.iter() {
             octasine
                 .sync
                 .set_parameter(parameter.to_index() as i32, (i % 64) as f32 / 64.0);
@@ -189,32 +197,29 @@ fn benchmark<A: AudioGen + Simd>(name: &str, expected_hash: &str) -> (bool, f32)
 
         octasine.update_audio_parameters();
 
-        let mut position = 0usize;
-
-        for (lefts, rights) in lefts
+        for (j, (lefts, rights)) in lefts
             .chunks_exact_mut(A::SAMPLES)
             .zip(rights.chunks_exact_mut(A::SAMPLES))
+            .enumerate()
         {
             unsafe {
-                A::process_f32(&mut octasine.audio, lefts, rights, position);
+                A::process_f32(&mut octasine.audio, lefts, rights, j * A::SAMPLES);
             }
-
-            position += A::SAMPLES;
         }
 
         for (l, r) in lefts.iter().zip(rights.iter()) {
-            results.update(&l.to_ne_bytes());
-            results.update(&r.to_ne_bytes());
+            output_hasher.update(&l.to_ne_bytes());
+            output_hasher.update(&r.to_ne_bytes());
         }
     }
 
     let elapsed = now.elapsed();
 
     let elapsed_millis = elapsed.as_millis();
-    let num_samples = SIZE * iterations;
-    let num_seconds = (SIZE * iterations) as f32 / 44100.0;
+    let num_samples = BUFFER_LEN * BUFFER_ITERATIONS;
+    let num_seconds = num_samples as f32 / 44100.0;
 
-    let processing_time_per_sample = (elapsed.as_nanos() as f32 / SIZE as f32) / iterations as f32;
+    let processing_time_per_sample = elapsed.as_nanos() as f32 / num_samples as f32;
 
     println!();
     println!(
@@ -236,16 +241,16 @@ fn benchmark<A: AudioGen + Simd>(name: &str, expected_hash: &str) -> (bool, f32)
         elapsed_millis as f32 / (num_seconds * 10.0)
     );
 
-    let result_hash: String = results
+    let output_hash: String = output_hasher
         .finalize()
         .iter()
         .take(8)
         .map(|byte| format!("{:02x} ", byte))
         .collect();
 
-    println!("Output hash (first 8 bytes):    {}", result_hash);
+    println!("Output hash (first 8 bytes):    {}", output_hash);
 
-    let success = result_hash == expected_hash;
+    let success = output_hash == expected_hash;
 
     let hash_match = if success { "yes".green() } else { "no".red() };
 
