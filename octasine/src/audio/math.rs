188aa8aff1d91f0f924f6def059356bc9b7bfaa8
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-05-20 10:16:28 +0200

    Add sin and cos functions linking to sleef

--- octasine/src/audio/math.rs
+++ octasine/src/audio/math.rs
@@ -2,3 +2,25 @@
 pub fn exp2(value: f32) -> f32 {
     fast_math::exp2_raw(value)
 }
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "simd")] {
+        pub fn sin(value: f32) -> f32 {
+            unsafe {
+                ::sleef_sys::Sleef_cinz_sinf1_u35purec(value)
+            }
+        }
+        pub fn cos(value: f32) -> f32 {
+            unsafe {
+                ::sleef_sys::Sleef_cinz_cosf1_u35purec(value)
+            }
+        }
+    } else {
+        pub fn sin(value: f32) -> f32 {
+            value.sin()
+        }
+        pub fn cos(value: f32) -> f32 {
+            value.cos()
+        }
+    }
+}
