9e76a0badac9e5ea09da36cbc714cb6bed39b849
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-05-30 21:01:29 +0200

    GUI: show envelope buttons in a grid

--- octasine/src/gui/interface/envelope/mod.rs
+++ octasine/src/gui/interface/envelope/mod.rs
@@ -1,8 +1,9 @@
 pub mod widget;
 
+use iced_baseview::alignment::Horizontal;
 use iced_baseview::tooltip::Position;
-use iced_baseview::Tooltip;
 use iced_baseview::{button, Alignment, Button, Column, Element, Length, Row, Space, Text};
+use iced_baseview::{Font, Tooltip};
 
 use crate::parameters::list::{OperatorParameter, Parameter};
 use crate::parameters::operator_envelope::OperatorEnvelopeLockGroupValue;
@@ -56,70 +57,44 @@ impl Envelope {
     }
 
     pub fn view(&mut self) -> Element<Message> {
-        let sync_viewports_message = Message::EnvelopeSyncViewports {
-            viewport_factor: self.widget.get_viewport_factor(),
-            x_offset: self.widget.get_x_offset(),
-        };
-
-        let zoom_out = Tooltip::new(
-            Button::new(
-                &mut self.zoom_out,
-                Text::new("−").font(self.style.font_bold()),
-            )
-            .on_press(Message::EnvelopeZoomOut(self.operator_index))
-            .padding(self.style.button_padding())
-            .style(self.style.button()),
+        let zoom_out = button_with_tooltip(
+            self.style,
+            &mut self.zoom_out,
+            self.style.font_bold(),
+            "−",
+            Message::EnvelopeZoomOut(self.operator_index),
             "Zoom out",
-            Position::Top,
-        )
-        .style(self.style.tooltip())
-        .font(self.style.font_regular())
-        .padding(self.style.tooltip_padding());
+        );
 
-        let zoom_in = Tooltip::new(
-            Button::new(
-                &mut self.zoom_in,
-                Text::new("+").font(self.style.font_bold()),
-            )
-            .on_press(Message::EnvelopeZoomIn(self.operator_index))
-            .padding(self.style.button_padding())
-            .style(self.style.button()),
+        let zoom_in = button_with_tooltip(
+            self.style,
+            &mut self.zoom_in,
+            self.style.font_bold(),
+            "+",
+            Message::EnvelopeZoomIn(self.operator_index),
             "Zoom in",
-            Position::Top,
-        )
-        .style(self.style.tooltip())
-        .font(self.style.font_regular())
-        .padding(self.style.tooltip_padding());
+        );
 
-        let fit = Tooltip::new(
-            Button::new(
-                &mut self.zoom_to_fit,
-                Text::new("FIT").font(self.style.font_regular()),
-            )
-            .on_press(Message::EnvelopeZoomToFit(self.operator_index))
-            .padding(self.style.button_padding())
-            .style(self.style.button()),
+        let fit = button_with_tooltip(
+            self.style,
+            &mut self.zoom_to_fit,
+            self.style.font_regular(),
+            "F",
+            Message::EnvelopeZoomToFit(self.operator_index),
             "Zoom to fit",
-            Position::Top,
-        )
-        .style(self.style.tooltip())
-        .font(self.style.font_regular())
-        .padding(self.style.tooltip_padding());
+        );
 
-        let distribute = Tooltip::new(
-            Button::new(
-                &mut self.sync_viewport,
-                Text::new("DIST").font(self.style.font_regular()),
-            )
-            .on_press(sync_viewports_message)
-            .padding(self.style.button_padding())
-            .style(self.style.button()),
-            "Distribute to other envelopes",
-            Position::Top,
-        )
-        .style(self.style.tooltip())
-        .font(self.style.font_regular())
-        .padding(self.style.tooltip_padding());
+        let distribute = button_with_tooltip(
+            self.style,
+            &mut self.sync_viewport,
+            self.style.font_regular(),
+            "D",
+            Message::EnvelopeSyncViewports {
+                viewport_factor: self.widget.get_viewport_factor(),
+                x_offset: self.widget.get_x_offset(),
+            },
+            "Distribute view to other envelopes",
+        );
 
         Row::new()
             .push(container_l3(self.style, self.widget.view()))
@@ -135,10 +110,42 @@ impl Envelope {
                             .push(zoom_in),
                     )
                     .push(Space::with_height(Length::Units(2)))
-                    .push(fit)
-                    .push(Space::with_height(Length::Units(2)))
-                    .push(distribute),
+                    .push(
+                        Row::new()
+                            .push(fit)
+                            .push(Space::with_width(Length::Units(3)))
+                            .push(distribute),
+                    ),
             ))
             .into()
     }
+}
+
+fn button_with_tooltip<'a>(
+    style: Theme,
+    button_state: &'a mut button::State,
+    button_font: Font,
+    button_text: &'static str,
+    button_message: Message,
+    tooltip_text: &'static str,
+) -> Element<'a, Message> {
+    Tooltip::new(
+        Button::new(
+            button_state,
+            Text::new(button_text)
+                .font(button_font)
+                .height(Length::Units(LINE_HEIGHT))
+                .width(Length::Units(LINE_HEIGHT / 2))
+                .horizontal_alignment(Horizontal::Center),
+        )
+        .on_press(button_message)
+        .padding(style.button_padding())
+        .style(style.button()),
+        tooltip_text,
+        Position::Top,
+    )
+    .style(style.tooltip())
+    .font(style.font_regular())
+    .padding(style.tooltip_padding())
+    .into()
 }
