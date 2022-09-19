cb9953c102e9f2d859741f32c77564d07d0764fe
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-09-19 21:05:33 +0200

    Add bhaskara sin and cos approximations

--- octasine/src/math.rs
+++ octasine/src/math.rs
@@ -28,3 +28,44 @@ cfg_if::cfg_if! {
         }
     }
 }
+
+/// Approximate cos(a * PI / 2) for range 0.0 to 1.0
+#[allow(dead_code)]
+pub fn bhaskara_cos_frac_pi_2(a: f32) -> f32 {
+    let a2 = a * a;
+
+    4.0 * ((1.0 - a2) / (4.0 + a2))
+}
+
+/// Approximate sin(a * PI / 2) for range 0.0 to 1.0
+#[allow(dead_code)]
+pub fn bhaskara_sin_frac_pi_2(a: f32) -> f32 {
+    bhaskara_cos_frac_pi_2(1.0 - a)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f32::consts::FRAC_PI_2;
+
+    use super::*;
+
+    #[test]
+    fn test_bhaskara() {
+        let precision = u16::MAX;
+
+        for i in 0..=precision {
+            let a = f32::from(i) / f32::from(precision);
+
+            assert_approx_eq::assert_approx_eq!(
+                bhaskara_cos_frac_pi_2(a),
+                (a * FRAC_PI_2).cos(),
+                0.005
+            );
+            assert_approx_eq::assert_approx_eq!(
+                bhaskara_sin_frac_pi_2(a),
+                (a * FRAC_PI_2).sin(),
+                0.005
+            );
+        }
+    }
+}
