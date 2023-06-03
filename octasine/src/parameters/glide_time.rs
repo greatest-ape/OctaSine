68602abd6ee35ef53cd271bb09e947a1b3eaaa4d
Joakim Frostegård <joakim.frostegard@gmail.com>
2023-06-03 19:27:15 +0200

    Change glide time steps

--- octasine/src/parameters/glide_time.rs
+++ octasine/src/parameters/glide_time.rs
@@ -5,7 +5,7 @@ use super::{
     ParameterValue, SerializableRepresentation,
 };
 
-const STEPS: &[f32] = &[0.0, 4.0];
+const STEPS: &[f32] = &[0.0, 1.0, 8.0];
 
 #[derive(Debug, Clone, Copy)]
 pub struct GlideTimeValue(f32);
